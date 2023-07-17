da16b59b9c179c272a36163c4796d752f48a891e
Adam Britt <adamrussellbritt@gmail.com>
2023-07-17 15:37:25 -0500

    final commit

--- js/script.js
+++ js/script.js
@@ -2,12 +2,29 @@ var button = document.querySelector(".got-this");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
-button.addEventListener("click", function () {
+function openModal() {
   modal.classList.add("show-modal");
   button.innerText = "You've got this!!!";
+}
+
+function closeModal() {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+}
+
+button.addEventListener("click", function () {
+  openModal();
 });
 
 modalX.addEventListener("click", function () {
-  modal.classList.remove("show-modal");
-  button.innerText = "Who's got this?";
+  closeModal();
+});
+
+document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+  if(e.key === 'Escape') {
+    if(modal.classList.contains(".show-modal")) {
+      closeModal();
+    }
+  }
 });
